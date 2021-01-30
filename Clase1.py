809e52f9516db89521cdc521d89ae42278f4bc4a
xrlie <c.aviles.rz@gmail.com>
2021-01-29 19:22:23 -0600

    ADD Clase1 python

---
+++ Clase1.py
@@ -0,0 +1,44 @@
+# Hola, te presento mi primer código
+
+# print(12+13)
+
+### Enteros
+print("Resultados Enteros")
+print("Suma de 2 + 2 =", 2 + 2)
+print("División de 1 / 3 =", 2 / 2)
+print("Resta de 2 - 2 =", 2 - 2)
+print("Multiplicación de 2 * 2 =", 2 * 2)
+print("División entera de 7 // 3 =", 2 // 2)
+print("Número elavado al número 3 ** 3 =", 3 ** 3)
+print("Módulo de 3 % 3 =", 3 % 3)
+print("Módulo de 5 % 3 =", 5 % 3)
+
+### Float
+print("")
+print("Resultados Flotantes")
+print("Suma de 2.4 + 2 =", 2. + 2)
+print("División de 1.4 / 3 =", 2. / 2)
+print("Resta de 2.4 - 2 =", 2. - 2)
+print("Multiplicación de 2.4 * 2 =", 2. * 2)
+print("División entera de 7.4 // 3 =", 2. // 2)
+print("Número elavado al número 3.4 ** 3 =", 3. ** 3)
+print("Módulo de 3.4 % 3 =", 3. % 3)
+print("Módulo de 5.4 % 3 =", 5. % 3)
+
+### String
+print("\n")
+print("")
+print("Concatenación: '3' + '3' ", "'3" + "3'")
+print('Concatenación: "3" + "3" ', '"3' + '3"')
+# print("Carlos Aviles Ruiz \n"*100)
+
+### Bool
+True
+False
+
+### Asignación múltiple
+x, y, z = 11, 12, 13
+
+### Otros operadores de asignación
+x = x + 1
+x += 1
