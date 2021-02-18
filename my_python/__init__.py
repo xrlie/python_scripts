4dca139b29c93c3f4a058a2c1e09b1f2ca6e1341
xrlie <c.aviles.rz@gmail.com>
2021-02-17 19:40:45 -0600

    ADD packages

---
+++ my_python/__init__.py
@@ -0,0 +1,11 @@
+def hola_mundo():
+    print('Hola Mundo')
+
+def tipo(objeto):
+    return type(objeto)
+
+def longitud(objeto):
+    return len(objeto)
+
+def musica_favorita():
+    return 'Rock Alternativo en Inlgés'
